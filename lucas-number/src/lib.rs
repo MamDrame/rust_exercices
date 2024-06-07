c5e5c740fc974696a66f829e0bdc3a4e75662d69
mamdrame <mamourkifa20@gmail.com>
2024-06-07 20:34:50 +0000

    Refactor lucas_number function for clarity

--- lucas-number/src/lib.rs
+++ lucas-number/src/lib.rs
@@ -1,19 +1,22 @@
 //This function receives a number n and returns the nth number in the Lucas Numbers where the nth number is the sum of the previous two numbers in the series.
 //
 // The Lucas Numbers start like this: 2, 1, 3, 4, 7, 11, 18, 29, 47, 76, 123, etc...
-pub fn lucas_number(n: u32) -> u32 {
-    return if n == 0 {
-        2
-    } else if n == 1 {
-        1
+const FIRST_LUCAS: u32 = 2;
+const SECOND_LUCAS: u32 = 1;
+
+pub fn lucas_number(lucas_position: u32) -> u32 {
+    if lucas_position == 0 {
+        FIRST_LUCAS
+    } else if lucas_position == 1 {
+        SECOND_LUCAS
     } else {
-        let mut a = 2;
-        let mut b = 1;
-        for _ in 2..=n {
-            let temp = a + b;
-            a = b;
-            b = temp;
+        let mut prev_number = FIRST_LUCAS;
+        let mut current_number = SECOND_LUCAS;
+        for _ in 2..=lucas_position {
+            let temp = prev_number + current_number;
+            prev_number = current_number;
+            current_number = temp;
         }
-        b
+        current_number
     }
 }

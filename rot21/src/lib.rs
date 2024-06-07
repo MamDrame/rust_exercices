fcf9b22d25d13d87e84ba1421230f28c8031607e
mamdrame <mamourkifa20@gmail.com>
2024-06-07 20:42:38 +0000

    Refactor rot21 function for efficiency

--- rot21/src/lib.rs
+++ rot21/src/lib.rs
@@ -1,23 +1,14 @@
+const LETTERS: &str = "abcdefghijklmnopqrstuvwxyz";
 pub fn rot21(input: &str) -> String {
-    let letters = "abcdefghijklmnopqrstuvwxyz";
-    let mut output = String::new();
-
-    for ch in input.chars() {
-        if !letters.contains(ch.to_ascii_lowercase()) {
-            output.push(ch);
-            continue;
-        };
-
-        let position = letters.chars().position(|c| c == ch.to_ascii_lowercase()).unwrap_or_default();
-        let corresponding_letter = letters.chars().nth((position + 21) % 26).unwrap_or_default();
-        // println!("rot char : {} position: {} c_position: {} letters: {}", ch, position, (position + 21) % 26, corresponding_letter);
-        
-        if ch.is_ascii_uppercase() {
-            output.push(corresponding_letter.to_ascii_uppercase());
-        } else {
-            output.push(corresponding_letter)
-        }
+    input.chars().map(|ch| {
+        if !LETTERS.contains(ch.to_ascii_lowercase()) {
+        return ch;
     };
-
-    output
+        let position = LETTERS.chars().position(|c| c == ch.to_ascii_lowercase()).unwrap_or_default();
+        let corresponding_letter = LETTERS.chars().nth((position + 21) % 26).unwrap_or_default();
+        if ch.is_ascii_uppercase() {
+            corresponding_letter.to_ascii_uppercase()
+        } else {
+            corresponding_letter
+        }}).collect()
 }

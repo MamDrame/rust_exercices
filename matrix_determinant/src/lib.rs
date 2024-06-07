10839b3534981370263c7b3d1a575fe2bca3f070
mamdrame <mamourkifa20@gmail.com>
2024-06-07 20:38:18 +0000

    Refactor variable naming in matrix determinant calculation

--- matrix_determinant/src/lib.rs
+++ matrix_determinant/src/lib.rs
@@ -1,12 +1,10 @@
 pub fn matrix_determinant(matrix: [[isize; 3]; 3]) -> isize {
-    let tl1 = matrix[0][0] * matrix[1][1] * matrix[2][2];
-    let tl2 = matrix[0][1] * matrix[1][2] * matrix[2][0];
-    let tl3 = matrix[0][2] * matrix[1][0] * matrix[2][1];
+    let diagonal_1_1 = matrix[0][0] * matrix[1][1] * matrix[2][2];
+    let diagonal_1_2 = matrix[0][1] * matrix[1][2] * matrix[2][0];
+    let diagonal_1_3 = matrix[0][2] * matrix[1][0] * matrix[2][1];
+    let diagonal_2_1 = matrix[0][2] * matrix[1][1] * matrix[2][0];
+    let diagonal_2_2 = matrix[0][1] * matrix[1][0] * matrix[2][2];
+    let diagonal_2_3 = matrix[0][0] * matrix[1][2] * matrix[2][1];
 
-    let tr1 = matrix[0][2] * matrix[1][1] * matrix[2][0];
-    let tr2 = matrix[0][1] * matrix[1][0] * matrix[2][2];
-    let tr3 = matrix[0][0] * matrix[1][2] * matrix[2][1];
-    
-    tl1 + tl2 + tl3 - (tr1 + tr2 + tr3)
+    diagonal_1_1 + diagonal_1_2 + diagonal_1_3 - (diagonal_2_1 + diagonal_2_2 + diagonal_2_3)
 }
-

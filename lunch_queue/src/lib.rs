a7d0813448c49f3bc919113d370c9efa47e27396
mamdrame <mamourkifa20@gmail.com>
2024-06-14 18:52:47 +0000

    Refactor code to improve Queue class methods

--- lunch_queue/src/lib.rs
+++ lunch_queue/src/lib.rs
@@ -1,93 +1,69 @@
 #[derive(Debug, Clone)]
 pub struct Queue {
-    pub node: Link,
+    pub node: PersonLink,
 }
 
-pub type Link = Option<Box<Person>>;
+pub type PersonLink = Option<Box<Person>>;
 
 #[derive(Debug, Clone)]
 pub struct Person {
     pub discount: i32,
     pub name: String,
-    pub next_person: Link,
+    pub next_person: PersonLink,
 }
 
 impl Queue {
     pub fn new() -> Queue {
         Self { node: None }
     }
-
     pub fn add(&mut self, name: String, discount: i32) {
         let new_node = Some(Box::new(Person {
             name,
             discount,
             next_person: self.node.take(),
         }));
-
         self.node = new_node;
     }
-
     pub fn invert_queue(&mut self) {
-        let mut a: Vec<Person> = Vec::new();
-        let mut current = self.node.take();
-
-        while let Some(person) = current.clone() {
-            let p = Person {
-                name: person.name,
-                discount: person.discount,
-                next_person: None,
-            };
-            a.push(p);
-            current = person.next_person;
-        }
-
+        let mut a: Vec<Person> = self.unroll_queue();
         let mut new_node = Self::new();
         for person in a {
             new_node.add(person.name, person.discount)
         }
-
         self.node = new_node.node;
-      
     }
-
     pub fn rm(&mut self) -> Option<(String, i32)> {
+        let mut a: Vec<Person> = self.unroll_queue();
+        let removed_person = a.pop();
+        a.reverse();
+        let mut new_node = Self::new();
+        for person in a {
+            new_node.add(person.name, person.discount)
+        }
+        self.node = new_node.node;
+        removed_person.map(|person| (person.name, person.discount))
+    }
+    fn unroll_queue(&mut self) -> Vec<Person> {
         let mut a: Vec<Person> = Vec::new();
         let mut current = self.node.take();
-
-        while let Some(person) = current.clone() {
-            let p = Person {
-                name: person.name,
+        while let Some(mut person) = current {
+            a.push(Person {
+                name: person.name.clone(),
                 discount: person.discount,
                 next_person: None,
-            };
-            a.push(p);
-            current = person.next_person;
+            });
+            current = person.next_person.take();
         }
-
-        let removed_person = a.pop();
-        let mut new_node = Self::new();
-        for person in a.iter().rev() {
-            new_node.add(person.name.clone(), person.discount)
-        }
-
-        self.node = new_node.node;
-
-        match removed_person {
-            Some(person) => Some((person.name, person.discount)),
-            None => None,
-        }
+        a
     }
-
     pub fn search(&self, name: &str) -> Option<(String, i32)> {
-        let mut p = self.node.clone();
-        while p.is_some() {
-            let a: Box<Person> = p.unwrap();
-            if a.name.clone() == name {
-                return Some((a.name.clone(), a.discount.clone()));
-            };
-            p = a.next_person;
+        let mut current = self.node.as_ref();
+        while let Some(person) = current {
+            if person.name == name {
+                return Some((person.name.clone(), person.discount));
+            }
+            current = person.next_person.as_ref();
         }
-
         None
     }
 }
